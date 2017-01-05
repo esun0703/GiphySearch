f7c677a4adeb24e2f584568d440d91de4afdad65
Emily Sun <ESun0703@Emilys-MacBook-Pro.local>
2017-01-05 18:08:21 -0500

    added buttongenerator function, added load gif function, added addGif on click.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,38 +1,59 @@
-//Generate Buttons
-var animes=["fairy tail", "dragonball",// "naruto", "black butler", "code geass", "inuyasha", "bleach"
-];
-
-
-for (i=0;i<animes.length;i++){
-	buttonGenerator(animes[i]);	
-}
-
-function buttonGenerator(animeName){
-	console.log(animeName);
-	var animeButton=$("<button>").text(animeName).attr("");
-	$("#buttonDisplay").append(button);
-	//animeButton.attr(
-		//text:"" set button's value and text to the word generted.
-	//});
-}
-
-//When Button Is Clicked
-$("button").on("click", function (){	//"this" keyword refers to when this button is clicked//
-	//takes the attribute of the button that was clicked.
-	var keyword=$(this).attr("data-anime");
-	console.log(this);
-
-	//Contructing a URL to search Giphy for the keyword that was clicked
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
-        keyword + "&api_key=dc6zaTOxFJmzC&limit=10";
-
-	$.ajax({
-	url:queryURL,
-	method:"GET"
-	})
-	.done(function(response){
-
-	});
+$( document ).ready(function() {
+    console.log( "ready!" );
 });
 
-//KeyWord Search To Make Buttons Appear
+//Generate Buttons
+var animes=["fairy tail", "dragonball","naruto", "black butler", "code geass", "inuyasha", "bleach"];
+console.log(animes);
+
+function buttonGenerator(){
+	for(i=0;i<animes.length;i++){
+		var b = $("<button>");
+		b.addClass("gif");
+		b.attr("data-name", animes[i]);
+		b.text(animes[i]);
+		$("#buttonDisplay").append(b);
+	};
+};
+
+buttonGenerator();
+
+$("#addGIF").on("click", function(event) {
+   event.preventDefault();
+   //this line will grab the input from the textbox
+   var buttonItem = $("#GIF-input").val().trim();
+   console.log(gifs);
+   gifs.push(buttonItem);
+   //calling renderButtons, which handles the processing of our movie array
+   renderButtons();
+});
+
+function loadGIF() {
+   $("#resultsArea").empty()
+   console.log("inside loadGIF");
+   var gifData = $(this).data("name");
+   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifData + "&api_key=dc6zaTOxFJmzC&limit=20";
+   $.ajax({
+       url: queryURL,
+       method: "GET"
+   })
+   .done(function(response) {
+       console.log(response)
+       var results = response.data;
+       for (var i = 0; i < results.length ; i++) {
+           var gifDiv = $("<div class='item'>");
+           var rating = response.data[i].rating;
+           var p = $("<p>").text("Rating: " + rating);
+           var gifImage = $("<img>");
+           gifImage.attr("src", response.data[i].images.fixed_height.url);
+           $("#resultsArea").prepend(gifDiv);
+           gifDiv.prepend(p);
+           gifDiv.prepend(gifImage);
+           
+       }
+   });
+}
+window.onload = function() {
+   $(document).on("click", ".gif", loadGIF);
+   
+}
