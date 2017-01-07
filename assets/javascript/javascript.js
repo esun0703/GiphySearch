cd1e9705ae9532a9c7cc9e6f8fc03fd984fdcb26
Emily Sun <ESun0703@Emilys-MacBook-Pro.local>
2017-01-07 16:54:12 -0500

    index.html added rows and container. style.css: added styling for everything except body,html, and jumbotron, and panel-heading. javascript.js: chnaged line 32, 9.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,14 +1,15 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-});
 
 //Generate Buttons
-var animes=["fairy tail", "dragonball","naruto", "black butler", "code geass", "inuyasha", "bleach"];
+var animes=["Fairy Tail", "Dragonball","Naruto", "Black Butler", "Code Geass", "Inuyasha", "Bleach", "Future Diary"];
 console.log(animes);
 
 function buttonGenerator(){
+	$("#buttonDisplay").empty();
 	for(i=0;i<animes.length;i++){
 		var b = $("<button>");
+		console.log(b);
 		b.addClass("gif");
 		b.attr("data-name", animes[i]);
 		b.text(animes[i]);
@@ -22,14 +23,13 @@ $("#addGIF").on("click", function(event) {
    event.preventDefault();
    //this line will grab the input from the textbox
    var buttonItem = $("#GIF-input").val().trim();
-   console.log(gifs);
-   gifs.push(buttonItem);
+   animes.push(buttonItem);
    //calling renderButtons, which handles the processing of our movie array
-   renderButtons();
+   buttonGenerator();
 });
 
 function loadGIF() {
-   $("#resultsArea").empty()
+   $("#giphyDisplayHere").empty()
    console.log("inside loadGIF");
    var gifData = $(this).data("name");
    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifData + "&api_key=dc6zaTOxFJmzC&limit=20";
@@ -42,18 +42,21 @@ function loadGIF() {
        var results = response.data;
        for (var i = 0; i < results.length ; i++) {
            var gifDiv = $("<div class='item'>");
-           var rating = response.data[i].rating;
+           var rating = results[i].rating;
            var p = $("<p>").text("Rating: " + rating);
            var gifImage = $("<img>");
-           gifImage.attr("src", response.data[i].images.fixed_height.url);
-           $("#resultsArea").prepend(gifDiv);
+           gifImage.attr("src", results[i].images.fixed_height.url);
+           gifImage.addClass("xs-col-3")
            gifDiv.prepend(p);
            gifDiv.prepend(gifImage);
-           
+           $("#giphyDisplayHere").prepend(gifDiv);      
        }
    });
 }
-window.onload = function() {
-   $(document).on("click", ".gif", loadGIF);
+
+$(document).on("click", ".gif", loadGIF);
    
-}
+
+
+});
+
